5b51afae0460641d673d7cea9c59a49f8dc9d7dd
radiks <radion91@gmail.com>
2016-06-24 11:28:54 +0300

    123213

--- src/js/develop/develop_4.js
+++ src/js/develop/develop_4.js
@@ -1,8 +1,13 @@
-
+function hideLeftMenu() {
+    $('.left-menu .hide-menu').click(function() {
+        $('.left-menu').stop().toggleClass('hide');
+        $('.global-wrapper .main').stop().toggleClass('hide-menu');
+    });
+}
 
 
 $(document).ready(function(){
-
+    hideLeftMenu();
 });
 
 $(window).load(function(){
