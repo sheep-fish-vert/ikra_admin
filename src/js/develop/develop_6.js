eecd1b81256f5152d3f9a9eadb8658b2b48ce9e9
Nazar <kryvyynazariy@gmail.com>
2016-06-29 18:02:42 +0300

    table,  ajax pager add

--- src/js/develop/develop_6.js
+++ src/js/develop/develop_6.js
@@ -1,7 +1,35 @@
+function pager(){
+	  $('#pager').on('click', "label", function(){
 
+	  $('#pager label').css('background','#eaeced');
+		var thisLabel = $(this);
+		thisLabel.css('background','transparent');	
+	  var num = $(this).attr('value');	
+	  var formSur = {
+	  	"action": "addContent",
+	  	"number": num
+	  };
+	    $.ajax({
+	        url : 'table.html',
+	        data: formSur,
+	        method:'POST',
+	        success : function(data){
+	      		$('#tableToSort tbody').remove();
+	      		$('#tableToSort').append(data);
 
+	        }
+	    });
+	})
+}
+
+function sorter(){
+	$("#tableToSort")
+		.tablesorter(); 
+}
 
 $(document).ready(function(){
+	sorter();
+	pager();
 
 });
 
