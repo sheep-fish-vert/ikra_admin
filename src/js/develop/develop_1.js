85f389dee850a97321a97f255bcac04b967ca4b9
Dmitry <dmitry1939@gmail.com>
2016-06-30 12:50:11 +0300

    offers checkpoint top & bottom search fields styled

--- src/js/develop/develop_1.js
+++ src/js/develop/develop_1.js
@@ -1,7 +1,27 @@
 
+/* search-line func */
 
+    function searchLineFunc(){
+
+        $('.style-it').each(function(){
+
+            var placeholder = {};
+
+            if($(this).attr('data-placeholder')){
+                placeholder.name = $(this).attr('data-placeholder');
+            }
+
+            $(this).styler(placeholder);
+
+        });
+
+    };
+
+/* /search-line func */
 
 $(document).ready(function(){
+
+    searchLineFunc();
 
 });
 
