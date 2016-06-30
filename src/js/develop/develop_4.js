246e15fed176eb7c8cdd5e02dae32054f2ac000c
Radion <radion91@gmail.com>
2016-06-30 17:52:00 +0300

    asdasdasd

--- src/js/develop/develop_4.js
+++ src/js/develop/develop_4.js
@@ -5,10 +5,24 @@ function hideLeftMenu() {
         $('.global-wrapper .header').stop().toggleClass('hide-menu');
     });
 }
+function arrowInLeftMenu(){
+    if( $('.left-menu-link ul li').hasClass('active') ) {
+        var arrow = '<span class="left-menu-mini-arrow"></span>';
+        $('.left-menu').after(arrow);
+        var activeLi = $('.left-menu-link ul li.active');
 
+        $('.left-menu-mini-arrow').css({
+            'top': activeLi.offset().top + activeLi.outerHeight()/2-8,// its a fucking magic
+            'left':$('.left-menu').width()
+        });
+    }else if( $('.left-menu-mini-arrow') ){
+        $('.left-menu-mini-arrow').remove();
+    }
+}
 
 $(document).ready(function(){
     hideLeftMenu();
+    arrowInLeftMenu();
 });
 
 $(window).load(function(){
